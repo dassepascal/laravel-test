4057dba223f61e1e49d6ff637b595c960bfd846a
Dasse <pascaldasse56@gmail.com>
2024-10-07 18:48:23 +0200

    suppression test css

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      
    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     {{-- bootstrap js --}}
@@ -16,7 +17,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-primary  ">
+<nav class="navbar navbar-expand-lg navbar-light bg-primary test ">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Agence</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
