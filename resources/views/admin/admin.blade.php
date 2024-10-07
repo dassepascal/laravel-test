8f31d3244a4f727e9d7fbb61c576ce4e0099df0b
Dasse <pascaldasse56@gmail.com>
2024-10-07 16:29:10 +0200

    add burger

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -3,24 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    
-    <title> @yield('title') | Administration </title>
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <title>@yield('title')</title>
 </head>
 <body>
 
-    <div class="container mt-5">
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        @yield('content')
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Mon Agence</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        @php
+                $route = Route::currentRouteName();
+          @endphp
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                 <li class="nav-item">
+                <a @class(['nav-link','active' => str_contains($route,'property.')]) href="{{ route('admin.property.index') }}">Gérer les biens</a>
+              </li>
+                 <li class="nav-item">
+                <a @class(['nav-link','active' => str_contains($route,'option.')]) href="{{ route('admin.option.index') }}">Gérer les options</a>
+              </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
-    
+</nav>
+<div class="container mt-5">
+    @if ($errors->any())
+<div class="alert alert-danger">
+    <ul class="list-group">
+        @foreach ($errors->all() as $error)
+        <li class="list-group-item">{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+    @yield('content')
+</div> 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ resources/views/admin/options/form.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.admin')
+
+@section('title', $option->exists ? 'Editer une option' : "Créer une option")
+
+@section('content')
+
+<div class="d-flex justify-content-between align-items-center">
+    <h1>@yield('title')</h1>
+</div>
+
+    <form
+        class="vstack gap-2"
+        action="{{ route($option->exists ? 'admin.option.update' : 'admin.option.store', ['option' => $option]) }}"
+        method="post">
+        @csrf
+
+        @method($option->exists ? 'PUT' : 'POST')
+
+
+
+        <div class="row">
+
+                @include('shared.input', ['class' => 'col','label' => 'Nom', 'name' => 'name', 'value' => $option->name])
+
+
+        </div>
+
+
+
+<div>
+    <button class="btn btn-primary">
+
+        @if($option->exists)
+        Modifier
+        @else
+        Créer
+        @endif
+    </button>
+
+</div>
+
+
+
+
+    </form>
+
+    @endsection
